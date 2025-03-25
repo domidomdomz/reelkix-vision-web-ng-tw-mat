c8e18216d033ef299f58c6e1d2380370476474c9
domidomdomz <domz.ciriaco@gmail.com>
2025-03-25 20:36:13 +0800

    Add environment.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://reelkix-vision-web-api.azurewebsites.net/api',
+    socialLinks: {
+      facebook: 'https://www.facebook.com/reelkix',
+      instagram: 'https://www.instagram.com/reelkix',
+      tiktok: 'https://www.tiktok.com/@reel.kix'
+    }
+  };
